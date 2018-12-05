707237672e54b7071de361536237b857af4208b0
shubham agrawal <shubhamsureshagrawal@Apples-MacBook-Pro.local>
2018-12-05 12:56:06 +0530

    Initial Commit

---
+++ inspireApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  inspireApp
+//
+//  Created by shubham suresh agrawal on 05/12/18.
+//  Copyright © 2018 shubham suresh agrawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
